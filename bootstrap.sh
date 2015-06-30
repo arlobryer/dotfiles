79b60db93fb8fc0c170ecd720eae7042ef1126d8
Arlo Bryer <abryer@palantir.com>
2015-06-30 08:38:04 +0100

    Fix the rsync command to take only dotfiles (previously going down ./.. in cwd)

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ git pull
 read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-  rsync --exclude ".git/" --exclude ".DS_Store" -avh ./.* ~
+  rsync --exclude ".git/" --exclude ".DS_Store" -avh ./.[^.]* ~
 fi
 echo "Installing Brew packages"
 sh brewinstall.sh
